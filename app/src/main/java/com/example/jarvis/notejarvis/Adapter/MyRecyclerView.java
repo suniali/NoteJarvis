d3cde136453fdbba2cb32cd1c3fc4296d472b863
AliMatroky <alimatroky@gmail.com>
2019-01-04 01:54:50 +0330

    create adapter and model of app module

---
+++ app/src/main/java/com/example/jarvis/notejarvis/Adapter/MyRecyclerView.java
@@ -0,0 +1,4 @@
+package com.example.jarvis.notejarvis.Adapter;
+
+public class MyRecyclerView {
+}
---
+++ app/src/main/java/com/example/jarvis/notejarvis/model/Note.java
@@ -0,0 +1,22 @@
+package com.example.jarvis.notejarvis.model;
+
+public class Note {
+    private Long date;
+    private String note;
+
+    public Long getDate() {
+        return date;
+    }
+
+    public void setDate(Long date) {
+        this.date = date;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+}
