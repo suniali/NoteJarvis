32610c8fdb844ca35ea57548904476e3099446fc
AliMatroky <alimatroky@gmail.com>
2019-01-04 02:48:02 +0330

    initialize GetNote and Intractor class in precenter constractor

--- app/src/main/java/com/example/jarvis/notejarvis/precenter/Precenter.java
+++ app/src/main/java/com/example/jarvis/notejarvis/precenter/Precenter.java
@@ -2,6 +2,7 @@ package com.example.jarvis.notejarvis.precenter;
 
 import android.content.Context;
 
+import com.example.data.sender.Intractor;
 import com.example.domain.Intractor.GetNotes;
 import com.example.domain.UseCase.IUseCase;
 import com.example.domain.model.DNote;
@@ -10,9 +11,11 @@ import java.util.List;
 
 public class Precenter implements IUseCase.callback<DNote> {
     private GetNotes getNotes;
+    private Intractor intractor;
     public Precenter(Context context)
     {
-
+        intractor=new Intractor(context);
+        getNotes=new GetNotes(intractor);
     }
     @Override
     public void getNotes(List<DNote> list) {
