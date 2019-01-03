efa98d527694140c59f41a4bdb69c03b628e8d90
AliMatroky <alimatroky@gmail.com>
2019-01-04 02:08:44 +0330

    create GetNotes core of usecase

---
+++ domain/src/main/java/com/example/domain/Intractor/GetNotes.java
@@ -0,0 +1,17 @@
+package com.example.domain.Intractor;
+
+import com.example.domain.UseCase.IUseCase;
+import com.example.domain.model.DNote;
+import com.example.domain.repository.IRepository;
+
+public class GetNotes implements IUseCase<DNote> {
+    private IRepository iRepository;
+    public GetNotes(IRepository iRepository)
+    {
+        this.iRepository=iRepository;
+    }
+    @Override
+    public void execute(callback callback) {
+        callback.getNotes(iRepository.getNotesFromApi());
+    }
+}
