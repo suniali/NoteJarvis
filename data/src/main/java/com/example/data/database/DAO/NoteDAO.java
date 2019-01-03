2ee9388a3137372fcf84aa13b52fbbf5037c6ff1
AliMatroky <alimatroky@gmail.com>
2019-01-04 02:42:52 +0330

    create insert method in NoteADO and call it in interface IRepository getDataForInsert

--- data/src/main/java/com/example/data/database/DAO/NoteDAO.java
+++ data/src/main/java/com/example/data/database/DAO/NoteDAO.java
@@ -1,10 +1,12 @@
 package com.example.data.database.DAO;
 
 import android.content.Context;
+import android.util.Log;
 
 import com.example.data.database.Interface.DaggerIDatabaseComponent;
 import com.example.data.database.module.ContextModule;
 import com.example.data.database.module.DatabaseModule;
+import com.example.data.model.ANote;
 
 import javax.inject.Inject;
 
@@ -21,5 +23,13 @@ public class NoteDAO {
                 .contextModule(new ContextModule(context))
                 .databaseModule(new DatabaseModule())
                 .build().injectRealm(this);
+    }
+    public void insertNote(ANote aNote)
+    {
+        realm.executeTransactionAsync(realm -> {
+            ANote objANote=realm.createObject(ANote.class,aNote.getNote());
+            objANote.setDate(aNote.getDate());
+        }, () -> Log.d("NoteDAO","inserting success ful to realm-------------------------"),
+                error -> Log.e("NoteDAO",error.getMessage()));
     }
 }
--- data/src/main/java/com/example/data/sender/Intractor.java
+++ data/src/main/java/com/example/data/sender/Intractor.java
@@ -2,15 +2,18 @@ package com.example.data.sender;
 
 import android.content.Context;
 
+import com.example.data.database.DAO.NoteDAO;
+import com.example.data.model.ANote;
 import com.example.domain.model.DNote;
 import com.example.domain.repository.IRepository;
 
 import java.util.List;
 
 public class Intractor implements IRepository {
+    private NoteDAO noteDAO;
     public Intractor(Context context)
     {
-        
+        noteDAO=new NoteDAO(context);
     }
     @Override
     public List<DNote> getNotesFromApi() {
@@ -23,5 +26,9 @@ public class Intractor implements IRepository {
     }
 
     private void insertToRealm(String note, Long date) {
+        ANote aNote=new ANote();
+        aNote.setDate(date);
+        aNote.setNote(note);
+        noteDAO.insertNote(aNote);
     }
 }
