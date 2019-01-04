75c3dcd075123dacad8e097c0d898a78e6faad2e
AliMatroky <alimatroky@gmail.com>
2019-01-04 17:12:52 +0330

    create IProvide data for get data from Precenter

---
+++ app/src/main/java/com/example/jarvis/notejarvis/precenter/IProvideData.java
@@ -0,0 +1,9 @@
+package com.example.jarvis.notejarvis.precenter;
+
+import com.example.jarvis.notejarvis.model.Note;
+
+import java.util.List;
+
+public interface IProvideData {
+    void getNotesFromPrecenter(List<Note> notes);
+}
