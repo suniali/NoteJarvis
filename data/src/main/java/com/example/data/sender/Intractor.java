24bbf1881b943a77633d832abef57565a6d76371
AliMatroky <alimatroky@gmail.com>
2019-01-04 03:54:49 +0330

    call getNotes method in getNotesFromApi in Irepository interface

--- data/src/main/java/com/example/data/sender/Intractor.java
+++ data/src/main/java/com/example/data/sender/Intractor.java
@@ -1,13 +1,18 @@
 package com.example.data.sender;
 
 import android.content.Context;
+import android.util.Log;
 
+import com.example.data.Mapper.MapToDNote;
 import com.example.data.database.DAO.NoteDAO;
 import com.example.data.model.ANote;
 import com.example.domain.model.DNote;
 import com.example.domain.repository.IRepository;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class Intractor implements IRepository {
     private NoteDAO noteDAO;
@@ -15,10 +20,26 @@ public class Intractor implements IRepository {
     {
         noteDAO=new NoteDAO(context);
     }
+
     @Override
     public List<DNote> getNotesFromApi() {
-        return null;
+        return mapANoteToDNote(noteDAO.getNotes());
     }
+
+    private List<DNote> mapANoteToDNote(List<ANote> notes) {
+        MapToDNote mapToDNote=new MapToDNote();
+        List<DNote> dNotes=new ArrayList<>();
+        for (ANote aNote:notes) {
+            DNote dNote=new DNote();    // TODO: 1/4/2019 why we can't use observable for ralm model and map othe class? 
+            dNote.setDate(aNote.getDate());
+            dNote.setNote(aNote.getNote());
+            dNotes.add(dNote);
+            Log.d("Intractor", "----------Intractor----------" + aNote.getNote());
+        }
+
+        return mapToDNote.transform(notes);
+    }
+
 
     @Override
     public void getDataForInsert(String note, Long date) {
--- domain/src/main/java/com/example/domain/Intractor/GetNotes.java
+++ domain/src/main/java/com/example/domain/Intractor/GetNotes.java
@@ -4,6 +4,9 @@ import com.example.domain.UseCase.IUseCase;
 import com.example.domain.model.DNote;
 import com.example.domain.repository.IRepository;
 
+import java.util.List;
+
+
 public class GetNotes implements IUseCase<DNote> {
     private IRepository iRepository;
     public GetNotes(IRepository iRepository)
@@ -12,6 +15,11 @@ public class GetNotes implements IUseCase<DNote> {
     }
     @Override
     public void execute(callback callback) {
+        List<DNote> list=iRepository.getNotesFromApi();
+        for (DNote dNote:list)
+        {
+            System.out.println("-------------use case--------------------"+dNote.getNote());
+        }
         callback.getNotes(iRepository.getNotesFromApi());
     }
 
