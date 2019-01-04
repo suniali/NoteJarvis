6b70753f71786c2dc89dd93ec47ed30e5a964c1f
AliMatroky <alimatroky@gmail.com>
2019-01-04 03:53:50 +0330

    create getnotes method for get notes from realm

--- data/src/main/java/com/example/data/database/DAO/NoteDAO.java
+++ data/src/main/java/com/example/data/database/DAO/NoteDAO.java
@@ -1,6 +1,7 @@
 package com.example.data.database.DAO;
 
 import android.content.Context;
+import android.os.Build;
 import android.util.Log;
 
 import com.example.data.database.Interface.DaggerIDatabaseComponent;
@@ -8,9 +9,14 @@ import com.example.data.database.module.ContextModule;
 import com.example.data.database.module.DatabaseModule;
 import com.example.data.model.ANote;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.inject.Inject;
 
+import io.reactivex.Observable;
 import io.realm.Realm;
+import io.realm.RealmResults;
 
 public class NoteDAO {
 
@@ -23,6 +29,30 @@ public class NoteDAO {
                 .contextModule(new ContextModule(context))
                 .databaseModule(new DatabaseModule())
                 .build().injectRealm(this);
+    }
+    public List<ANote> getNotes()
+    {
+        RealmResults<ANote> results=realm.where(ANote.class).findAll();
+        List<ANote> aNotes=new ArrayList<>();
+        if(results!=null)
+        {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                results.parallelStream()
+                        .forEach(aNote -> {
+                            aNotes.add(aNote);
+                        });
+            }else
+            {
+                for (ANote aNote:results)
+                {
+                    aNotes.add(aNote);
+                }
+            }
+            Log.d("NoteDAO","Fill list success full...........................");
+            return aNotes;
+        }
+        Log.e("NoteDAO","rezult was null!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        return null;
     }
     public void insertNote(ANote aNote)
     {
