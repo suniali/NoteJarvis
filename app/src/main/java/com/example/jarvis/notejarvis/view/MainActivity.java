b031370416ac25e0e635c9ad8178018928c062f7
AliMatroky <alimatroky@gmail.com>
2019-01-04 17:13:31 +0330

    impliment IProvide interface and get list and setup recycler view.

--- app/src/main/java/com/example/jarvis/notejarvis/view/MainActivity.java
+++ app/src/main/java/com/example/jarvis/notejarvis/view/MainActivity.java
@@ -3,20 +3,36 @@ package com.example.jarvis.notejarvis.view;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.EditText;
 
+import com.example.jarvis.notejarvis.Adapter.MyRecyclerView;
 import com.example.jarvis.notejarvis.R;
+import com.example.jarvis.notejarvis.model.Note;
+import com.example.jarvis.notejarvis.precenter.IProvideData;
 import com.example.jarvis.notejarvis.precenter.Precenter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements IProvideData {
 
     @BindView(R.id.recycler_note)
     RecyclerView recycler_note;
@@ -24,23 +40,36 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.txt_note)
     EditText txt_note;
     private Precenter precenter;
+    private MyRecyclerView myRecyclerView;
+    public List<Note> noteList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        noteList=new ArrayList<>();
+        Note note=new Note();
         ButterKnife.bind(this);
 
-        precenter=new Precenter(this);
+        precenter = new Precenter(this, this);
+
 
     }
+
+    private void setupRecycler(List<Note> notes) {
+        myRecyclerView = new MyRecyclerView(this, notes);
+        recycler_note.setLayoutManager(new LinearLayoutManager(this));
+        recycler_note.setAdapter(myRecyclerView);
+    }
+
     @OnClick(R.id.img_add)
-    public void onClick_Img_Add()
-    {
-        String str_note=txt_note.getText().toString();
-        Long date=System.currentTimeMillis();
-        precenter.GetDataFromViewForInsert(str_note,date);
+    public void onClick_Img_Add() {
+        String str_note = txt_note.getText().toString();
+        Long date = System.currentTimeMillis();
+        precenter.GetDataFromViewForInsert(str_note, date);
+
+        precenter.refreshData();
+
     }
 
 
@@ -85,5 +114,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onBackPressed() {
         super.onBackPressed();
+    }
+
+    @Override
+    public void getNotesFromPrecenter(List<Note> notes) {
+        for (Note note : notes) {
+            noteList.add(note);
+            Log.d("MainActivity", "-------------------MainActivity----------" + note.getNote());
+        }
+        setupRecycler(notes);
     }
 }
